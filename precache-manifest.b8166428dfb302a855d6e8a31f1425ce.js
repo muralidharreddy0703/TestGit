65f44ed7762de0aa9f21a8ce8a738e65ffb36f4b
Muralidhar <muralidharreddy80@gmail.com>
2020-05-05 15:39:53 +0530

    Updates

---
+++ precache-manifest.b8166428dfb302a855d6e8a31f1425ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d44050f49ebba4584c476154b73114",
+    "url": "/muralidharreddy0703/TestGit/index.html"
+  },
+  {
+    "revision": "a4a22a161a9ccf0aaf36",
+    "url": "/muralidharreddy0703/TestGit/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dcb390dc08337c4bb7d2",
+    "url": "/muralidharreddy0703/TestGit/static/js/2.9401d5ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/muralidharreddy0703/TestGit/static/js/2.9401d5ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a22a161a9ccf0aaf36",
+    "url": "/muralidharreddy0703/TestGit/static/js/main.ac020043.chunk.js"
+  },
+  {
+    "revision": "74572c16e18031b9f71a",
+    "url": "/muralidharreddy0703/TestGit/static/js/runtime-main.d969f290.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/muralidharreddy0703/TestGit/static/media/logo.5d5d9eef.svg"
+  }
+]);
